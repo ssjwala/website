85aeb7751114b5d1216dd0988af2bf0fa3846818
sjwala <jwalasingh03882@gmail.com>
2021-10-24 16:40:50 +0530

    start

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+// $('.food-slider').slick({
+// autoplay:true,
+// slidesToShow:3,
+// slidesToScroll:1,
+// prevArrow:".prev-btn",
+// nextArrow:".next-btn",
+// });
+
+// });
+
+
+    $(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
+     
+    });
